ca8536d42abd8335177e1b3710bcb7204f7378e5
Shambhavi Singh <shamvi050801@gmail.com>
2023-04-11 23:53:09 +0530

    Update nt.cpp

--- nt.cpp
+++ nt.cpp
@@ -11,10 +11,8 @@
  POWER METHOD
  LAGRANGE INTERPOLATION
  NEWTON DIVIDED DIFFERENCE
-
-
-
-
+ simphson 1/3
+ trapezoidal
 
 
 
@@ -862,4 +860,101 @@ int main()
     cin >> ans;
     }while((ans == 'Y') || (ans == 'y'));
     return 0;
+}
+
+//--------------------------------------------------------------------------- (14)
+//simphson 1/3 rule
+
+
+#include<iostream>
+#include<math.h>
+
+/* Define function here */
+#define f(x) 1/(1+pow(x,2))
+
+using namespace std;
+int main()
+{
+ float lower, upper, integration=0.0, stepSize, k;
+ int i, subInterval;
+
+ /* Input */
+ cout<<"Enter lower limit of integration: ";
+ cin>>lower;
+ cout<<"Enter upper limit of integration: ";
+ cin>>upper;
+ cout<<"Enter number of sub intervals: ";
+ cin>>subInterval;
+
+ /* Calculation */
+
+ /* Finding step size */
+ stepSize = (upper - lower)/subInterval;
+
+ /* Finding Integration Value */
+ integration = f(lower) + f(upper);
+
+ for(i=1; i<= subInterval-1; i++)
+ {
+  k = lower + i*stepSize;
+
+  if(i%2==0)
+  {
+    integration = integration + 2 * (f(k));
+  }
+  else
+  {
+    integration = integration + 4 * (f(k));
+  }
+
+ }
+
+ integration = integration * stepSize/3;
+
+ cout<< endl <<"Required value of integration is: "<< integration;
+
+ return 0;
+}
+
+//-------------------------------------------------------------------------------  (15)
+//trapezoidal rule
+#include<iostream>
+#include<math.h>
+
+/* Define function here */
+#define f(x) 1/(1+pow(x,2))
+
+using namespace std;
+int main()
+{
+ float lower, upper, integration=0.0, stepSize, k;
+ int i, subInterval;
+
+ /* Input */
+ cout<<"Enter lower limit of integration: ";
+ cin>>lower;
+ cout<<"Enter upper limit of integration: ";
+ cin>>upper;
+ cout<<"Enter number of sub intervals: ";
+ cin>>subInterval;
+
+ /* Calculation */
+
+ /* Finding step size */
+ stepSize = (upper - lower)/subInterval;
+
+ /* Finding Integration Value */
+ integration = f(lower) + f(upper);
+
+ for(i=1; i<= subInterval-1; i++)
+ {
+  k = lower + i*stepSize;
+  integration = integration + 2 * (f(k));
+ }
+
+ integration = integration * stepSize/2;
+
+ cout<< endl<<"Required value of integration is: "<< integration;
+
+ return 0;
 }
